c100895e11db7c4aeac2435bcc5181e7d4ad29c5
Joaquin Gonzalez <joagonzalez@gmail.com>
2024-09-10 20:15:04 -0300

    refactor dispather for model execution and metrics decorator

--- main.py
+++ main.py
@@ -1,15 +1,69 @@
 import sys
 import time
 import tracemalloc
+from typing import Callable
 from src.simulator import simulation_hanoi
 from src.hanoi_states import StatesHanoi, ProblemHanoi
 from src.tree_hanoi import NodeHanoi
-from src.search import (  # Importa las funciones de búsqueda del módulo search
+from src.search import (
     breadth_first_tree_search,
     breadth_first_graph_search
 )
 
-def main():
+
+def metrics(func):
+    def wrapper(*args, **kwargs):
+        start_time = time.perf_counter()  # Start the timer
+        tracemalloc.start()
+        
+        result = func(*args, **kwargs)  # Call the original function
+        
+        end_time = time.perf_counter()  # Stop the timer
+        _, memory_peak = tracemalloc.get_traced_memory()
+        memory_peak /= 1024*1024
+        tracemalloc.stop()
+        execution_time = end_time - start_time
+        
+        print(f"Tiempo que demoró {func.__name__}: {execution_time:4f} [s]", )
+        print(f"Maxima memoria ocupada: {round(memory_peak, 2)} [MB]", )
+        
+        # TODO: Insert data in DB for futher analysis
+        return result
+    return wrapper
+
+@metrics
+def execute_algorithm(problem_hanoi: ProblemHanoi, solver: Callable) ->  NodeHanoi:
+    # Resuelve el problema utilizando búsqueda en anchura
+    # Esta forma de búsqueda es muy ineficiente, por lo que si deseas probarlo, usa 3 discos o si querés esperar
+    # un poco más, 4 discos, pero 5 discos no finaliza nunca.
+    #last_node = breadth_first_tree_search(problem_hanoi)
+    # Resuelve el problema utilizando búsqueda en anchura, pero con memoria que recuerda caminos ya recorridos.
+    last_node = solver(problem_hanoi, display=True)
+    return last_node
+
+def solve_problem(name: str, problem_hanoi: ProblemHanoi, solver: Callable) -> None:
+    print(f'-'*50)
+    print(f'Solving problem using {name}')
+    
+    last_node = execute_algorithm(problem_hanoi, breadth_first_graph_search)
+
+    if isinstance(last_node, NodeHanoi):
+        # Imprime la longitud del camino de la solución encontrada
+        print(f'Longitud del camino de la solución: {last_node.state.accumulated_cost}')
+
+        # Genera los JSON para el simulador
+        last_node.generate_solution_for_simulator()
+    else:
+        print(last_node)
+        print("No se encuentra solución")
+        
+def simulate() -> None:
+    """
+    Función que simula la solución del problema de la Torre de Hanoi.
+    """
+    simulation_hanoi.main()
+
+def main() -> None:
     """
     Función principal que resuelve el problema de la Torre de Hanoi y genera los JSON para el simulador.
     """
@@ -20,49 +74,14 @@ def main():
     # Crea una instancia del problema de la Torre de Hanoi
     problem_hanoi = ProblemHanoi(initial=initial_state, goal=goal_state)
 
-    # Para medir tiempo consumido
-    start_time = time.perf_counter()
-    # Para medir memoria consumida (usamos el pico de memoria)
-    tracemalloc.start()
-
-    # Métodos no informados
-
-    # Resuelve el problema utilizando búsqueda en anchura
-    # Esta forma de búsqueda es muy ineficiente, por lo que si deseas probarlo, usa 3 discos o si querés esperar
-    # un poco más, 4 discos, pero 5 discos no finaliza nunca.
-    #last_node = breadth_first_tree_search(problem_hanoi)
-
-    # Resuelve el problema utilizando búsqueda en anchura, pero con memoria que recuerda caminos ya recorridos.
-    last_node = breadth_first_graph_search(problem_hanoi, display=True)
-
-    _, memory_peak = tracemalloc.get_traced_memory()
-    memory_peak /= 1024*1024
-    tracemalloc.stop()
-
-    end_time = time.perf_counter()
-    elapsed_time = end_time - start_time
-
-    if isinstance(last_node, NodeHanoi):
-        # Imprime la longitud del camino de la solución encontrada
-        print(f'Longitud del camino de la solución: {last_node.state.accumulated_cost}')
-
-        # Genera los JSON para el simulador
-        last_node.generate_solution_for_simulator()
-
-    else:
-        print(last_node)
-        print("No se encuentra solución")
-
-    # Imprime las métricas medidas
-    print(f"Tiempo que demoró: {elapsed_time} [s]", )
-    print(f"Maxima memoria ocupada: {round(memory_peak, 2)} [MB]", )
-
-def simulate():
-    """
-    Función que simula la solución del problema de la Torre de Hanoi.
-    """
-    simulation_hanoi.main()
-
+    problems = {
+        'breadth_first_tree_search': breadth_first_tree_search,
+        'breadth_first_graph_search2': breadth_first_graph_search
+    }
+        
+    for name, search in problems.items():
+        solve_problem(name, problem_hanoi, search)
+        
 
 if __name__ == "__main__":
     """
