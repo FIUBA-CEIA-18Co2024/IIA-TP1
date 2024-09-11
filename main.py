fe07eeeb500b7513e1e760189e63b031c1117606
Joaquin Gonzalez <joagonzalez@gmail.com>
2024-09-11 18:19:39 -0300

    add psycop2 requirement and disks+model name in metrics

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import sys
 import time
 import tracemalloc
+from datetime import datetime
 from typing import Callable
 from src.simulator import simulation_hanoi
 from src.hanoi_states import StatesHanoi, ProblemHanoi
@@ -32,8 +33,9 @@ def metrics(func):
         if 'db' in sys.argv[1]:
             db = DatabaseService()           
             db.add(Metrics(
-                model_name="breadth_first_graph_search",
-                disks=5,
+                model_name=args[0],  # Add the model name as a parameter
+                disks=args[1],
+                timestamp=datetime.now(),
                 memory_allocation=memory_peak,
                 execution_time=execution_time,
                 comments="",
@@ -45,7 +47,7 @@ def metrics(func):
     return wrapper
 
 @metrics
-def execute_algorithm(problem_hanoi: ProblemHanoi, solver: Callable) ->  NodeHanoi:
+def execute_algorithm(name: str, disks: int, problem_hanoi: ProblemHanoi, solver: Callable) ->  NodeHanoi:
     # Resuelve el problema utilizando búsqueda en anchura
     # Esta forma de búsqueda es muy ineficiente, por lo que si deseas probarlo, usa 3 discos o si querés esperar
     # un poco más, 4 discos, pero 5 discos no finaliza nunca.
@@ -54,7 +56,7 @@ def execute_algorithm(problem_hanoi: ProblemHanoi, solver: Callable) ->  NodeHan
     last_node = solver(problem_hanoi, display=True)
     return last_node
 
-def solve_problem(name: str, problem_hanoi: ProblemHanoi, solver: Callable) -> None:
+def solve_problem(name: str, disks: int, problem_hanoi: ProblemHanoi, solver: Callable) -> None:
     """
     Función que resuelve el problema de la Torre de Hanoi utilizando un algoritmo de búsqueda.
 
@@ -66,7 +68,7 @@ def solve_problem(name: str, problem_hanoi: ProblemHanoi, solver: Callable) -> N
     print(f'-'*50)
     print(f'Solving problem using {name}')
     
-    last_node = execute_algorithm(problem_hanoi, breadth_first_graph_search)
+    last_node = execute_algorithm(name, disks, problem_hanoi, breadth_first_graph_search)
 
     if isinstance(last_node, NodeHanoi):
         # Imprimimos la longitud del camino de la solución encontrada
@@ -92,8 +94,9 @@ def main() -> None:
     Función principal que resuelve el problema de la Torre de Hanoi y genera los JSON para el simulador.
     """
     # Definimos estado inicial y estado final del problema a resolver
-    initial_state = StatesHanoi([5, 4, 3, 2, 1], [], [], max_disks=5)
-    goal_state = StatesHanoi([], [], [5, 4, 3, 2, 1], max_disks=5)
+    disks = 5
+    initial_state = StatesHanoi([5, 4, 3, 2, 1], [], [], max_disks=disks)
+    goal_state = StatesHanoi([], [], [5, 4, 3, 2, 1], max_disks=disks)
 
     # Se crea una instancia del problema de la Torre de Hanoi
     problem_hanoi = ProblemHanoi(initial=initial_state, goal=goal_state)
@@ -106,7 +109,7 @@ def main() -> None:
         
     # Se resuelve el problema para cada algoritmo de búsqueda
     for name, search in problems.items():
-        solve_problem(name, problem_hanoi, search)
+        solve_problem(name, disks, problem_hanoi, search)
         
 
 if __name__ == "__main__":
