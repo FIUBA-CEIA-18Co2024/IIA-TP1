64b1a6b4a84bc1ef98e154909a6418f9c0be93e1
Joaquin Gonzalez <joagonzalez@gmail.com>
2024-09-11 00:34:07 -0300

    add metrics model

---
+++ src/models/metrics.py
@@ -0,0 +1,23 @@
+from src.services.databases import Base
+from src.config.settings import config
+from sqlalchemy import Column, Integer, String, JSON, Float, DateTime, Boolean
+
+
+
+class Metrics(Base):
+    __tablename__ = config["DATABASE_TABLE_METRICS"]
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    timestamp = Column(DateTime, unique=True)
+    model_name = Column(String(200))
+    disks = Column(Integer)
+    memory_allocation = Column(Float)
+    execution_time = Column(Float)
+    comments = Column(String(1000))
+    
+    def __repr__(self):
+        return f"<{self.__class__.__name__} id: '{self.id}' model_name: '{self.model_name}'  timestamp: '{self.timestamp}'>"
+
+    def __str__(self):
+        return self.__repr__()
+    
